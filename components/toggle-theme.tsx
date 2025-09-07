cf10b50027e0f95224f333124bc6846ddb89d5ce
DengreSarthak <dengresarthak2002@gmail.com>
2025-09-07 17:45:15 +0530

    Fixed Favicon duplication and hydration error

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -6,11 +6,41 @@ import { useTheme } from 'next-themes'
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
 
   function toggleTheme() {
     const newTheme = theme === 'light' ? 'dark' : 'light'
     localStorage.setItem('color-scheme', newTheme)
     setTheme(newTheme)
+  }
+
+  if (!mounted) {
+    return (
+      <div
+        className="flex items-center space-x-4"
+        aria-label="Toggle theme"
+        role="button"
+      >
+        <div className="flex items-center w-20 h-10 rounded-full bg-gray-300 relative p-1 cursor-pointer transition-colors">
+          <div className="w-8 h-8 rounded-full shadow-md bg-white"></div>
+          <Sun
+            className="absolute left-2 top-1/2 transform -translate-y-1/2 opacity-100"
+            size={24}
+            color="orange"
+          />
+          <Moon
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 opacity-0"
+            size={24} 
+            color="yellow"
+          />
+        </div>
+      </div>
+    )
   }
 
   return (
